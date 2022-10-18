e5b6ab97ba969bc7a13808123ab6ce8f10be0c79
yerin-ddadda <yerin0110@naver.com>
2022-10-18 16:55:26 +0900

    iu member branch에서 MemberVO id, register

--- webstudy88-yrk1018/src/main/java/org/kosta/myproject/model/MemberVO.java
+++ webstudy88-yrk1018/src/main/java/org/kosta/myproject/model/MemberVO.java
@@ -4,4 +4,14 @@ import java.io.Serializable;
 
 public class MemberVO implements Serializable{
 
+	private static final long serialVersionUID = -6079971115987091112L;
+	private String id;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
 }
